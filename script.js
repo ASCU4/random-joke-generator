1a874d5f613313fc95de8ae559431a09e26a35a2
Shivansh Thakur <160780404+ASCU4@users.noreply.github.com>
2026-01-18 21:59:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const jokecontainer = document.getElementById("joke");
+const button = document.getElementById("button");
+const url ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single";
+
+let getJoke = () => {
+    jokecontainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        jokecontainer.textContent = `${item.joke}`;
+        jokecontainer.classList.add("fade");
+    });
+}
+button.addEventListener("click",getJoke);
+getJoke();
